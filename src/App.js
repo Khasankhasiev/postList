0b9cf35fabe7ed707d68c9692d97db3a081b5a39
Khasan <mr.khasiev@list.ru>
2024-09-18 20:59:26 +0300

    Создал проект React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let [likes, setLikes] = useState(0);
+    let [value, setValue] = useState('');
+
+    function increment() {
+        setLikes((likes += 1));
+    }
+    function decrement() {
+        setLikes((likes -= 1));
+    }
+    return (
+        <div className="App">
+            <h1>{likes}</h1>
+            <h1>{isNaN(Number(value)) === false ? value : 'enter a number'}</h1>
+            <button onClick={increment}>increment</button>
+            <button onClick={decrement}>decrement</button>
+            <input
+                type="text"
+                value={value}
+                onChange={event => setValue(event.target.value)}
+            />
+        </div>
+    );
 }
 
 export default App;
