1321027806267919aee8971e2515b84e1ae08324
Khasan <mr.khasiev@list.ru>
2024-09-19 20:21:04 +0300

    создание 2 компонентов счетчика

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
+import Counter from './components/Counter';
+import ClassCounter from './components/ClassCounter';
 
 function App() {
-    let [likes, setLikes] = useState(0);
-    let [value, setValue] = useState('');
-
-    function increment() {
-        setLikes((likes += 1));
-    }
-    function decrement() {
-        setLikes((likes -= 1));
-    }
     return (
         <div className="App">
-            <h1>{likes}</h1>
-            <h1>{isNaN(Number(value)) === false ? value : 'enter a number'}</h1>
-            <button onClick={increment}>increment</button>
-            <button onClick={decrement}>decrement</button>
-            <input
-                type="text"
-                value={value}
-                onChange={event => setValue(event.target.value)}
-            />
+            <Counter />
+            <ClassCounter></ClassCounter>
         </div>
     );
 }
