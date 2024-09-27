6009234051384a951ff04e1d6682433b29bf1194
Khasan <mr.khasiev@list.ru>
2024-09-27 19:37:51 +0300

    добавлен компонент модального окна и анимация списков

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React, { useMemo, useState } from 'react';
 import './styles/App.css';
 import PostList from './components/PostList';
 import PostForm from './components/PostForm';
-
 import PostFilter from './components/PostFilter';
+
+import MyModal from './components/UI/modal/MyModal';
+import MyButton from './components/UI/button/MyButton';
 
 function App() {
     const [posts, setPosts] = useState([
@@ -24,6 +26,7 @@ function App() {
         },
     ]);
     const [filter, setFilter] = useState({ sort: '', query: '' });
+    const [modal, setModal] = useState(false);
 
     const sortedPosts = useMemo(() => {
         if (filter.sort) {
@@ -43,6 +46,7 @@ function App() {
 
     const createPost = newPost => {
         setPosts([...posts, newPost]);
+        setModal(false);
     };
 
     const removePost = post => {
@@ -51,8 +55,13 @@ function App() {
 
     return (
         <div className="App">
-            <PostForm createPost={createPost} />
-            <hr style={{ margin: '15px 0' }} />
+            <MyButton style={{ marginTop: 30 }} onClick={() => setModal(true)}>
+                Создать пользователя
+            </MyButton>
+            <hr style={{ marginTop: 20, marginBottom: 20 }} />
+            <MyModal visible={modal} setVisible={setModal}>
+                <PostForm createPost={createPost} />
+            </MyModal>
             <PostFilter filter={filter} setFilter={setFilter} />
 
             <PostList
